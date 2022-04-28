6b8152bf74fb39dd41c33fac8e0bb5dac54fa13b
cjdomacena <cjdomacena@yahoo.com>
2022-04-28 00:29:15 -0400

    feat: :sparkles: Added api for accounts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import Dashboard from "./pages/Dashboard";
 import {Routes, Route} from 'react-router-dom'
 import Navbar from "./components/Navbar";
-import { useAppSelector } from "./redux/hooks";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
 import Nav from "./components/Navbar/Nav";
+import { useEffect, useState } from "react";
+import { initialAccount } from "./redux/AccountSlice";
 
 
 
 function App() {
 
-  const {isDarkMode} = useAppSelector(state => state.theme)
+  const {isDarkMode} = useAppSelector(state => state.theme);
+  const {user} = useAppSelector(state => state.accounts)
+  const dispatch = useAppDispatch();
+
+  async function setUserAccounts() {
+    try {
+      const req = await fetch(
+        'https://my-json-server.typicode.com/cjdomacena/IS322-Final-Project/user',
+      );
+      const res = await req.json();
+      dispatch(initialAccount(res))
+    }
+     catch(e:any) {
+        console.error(e.message || e.description)
+     }
+  }
+
+  useEffect(() => {
+    setUserAccounts();
+  }, [dispatch]);
 
   return (
     <div className={isDarkMode ? 'dark' : ''}>
--- src/redux/AccountSlice.ts
+++ src/redux/AccountSlice.ts
@@ -1,24 +1,65 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import type { RootState } from './store';
 
-interface TransactionState {
-
+interface IAccount {
+  account_id: string;
+  balance: string;
+  transactions: ITransactions[]
 }
 
-const initialState: TransactionState = {
-
+interface ITransactions {
+	amount: number,
+	origin: string,
+	destination: string,
+	description: string,
+	type: string
 }
+
+interface IUser  {
+	_id: string,
+	name: string,
+	email: string,
+	phone: string,
+	address: string,
+	accounts: IAccount[]
+}
+
+
+
+
+const initialState: {user: IUser} | null = {
+  user: {_id: '',
+  name: '',
+  email: '',
+  phone: '',
+  address: '',
+  accounts: [
+    {
+      account_id: '',
+      balance: '',
+      transactions: [
+        {
+          amount: 0.00,
+          origin: "",
+          destination: "",
+          description: "",
+          type: "",
+        },
+      ],
+    },
+  ],}
+};
 
 export const accountSlice = createSlice({
 	name: 'transactions',
 	initialState, 
 	reducers: {
-		updateTransaction: (state, action:PayloadAction<number>) => {
-			
+		initialAccount: (state, action: PayloadAction<IUser>) => {
+			state.user = {...action.payload}
 		}
 	}
 })
 
-export const {updateTransaction} = accountSlice.actions;
+export const { initialAccount } = accountSlice.actions;
 
 export default accountSlice.reducer;
